4fe632a25409d5ac052ae79d6ee46ed3cfbebccb
Johannes Feldmann <j.feldmann@software-and-service.com>
2017-05-11 15:54:46 +0200

    First very simple implementation of a median filter.

--- median.cpp
+++ median.cpp
@@ -9,10 +9,33 @@
 
 #include<image.h>
 
+#include <set>
+using std::multiset;
+
 void medianFilter(const unsigned char input[][HEIGHT], unsigned char output[][HEIGHT], unsigned int width, unsigned int height)
 {
+	multiset<unsigned char> pixel;
+	for (unsigned x = 1; x < (width-1); x++)
+	{
+		for (unsigned y = 1; y < (height-1); y++)
+		{
+			// clear set
+			pixel.clear();
 
-    //TODO: put your implementation here
+			// insert all pixel values
+			pixel.insert(input[x-1][y-1]);
+			pixel.insert(input[x-1][y]);
+			pixel.insert(input[x-1][y+1]);
+			pixel.insert(input[x][y-1]);
+			pixel.insert(input[x][y]);
+			pixel.insert(input[x][y+1]);
+			pixel.insert(input[x+1][y-1]);
+			pixel.insert(input[x+1][y]);
+			pixel.insert(input[x+1][y+1]);
 
+			// Get value in the mid of set
+			output[x][y] = *std::next(pixel.begin(), 4);
+		}
+	}
 }
 
